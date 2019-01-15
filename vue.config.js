c5ade66ac9ef765235f9f658dbceee1cd7d7a15d
AndresMRM <andres@inventati.org>
2019-01-15 07:32:25 -0200

    prepare deploy netlify

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/direitos-e-silencios/'
-    : '/',
   chainWebpack: config => {
     config.module
       .rule('yaml')
