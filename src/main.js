369f3534b5cbc1e75376809d5e0f910df04abc08
AndresMRM <andres@inventati.org>
2019-02-21 21:50:04 -0300

    matomo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueMatomo from 'vue-matomo'
 import App from '@/App.vue'
 import btnX from '@/components/customButton.vue'
 import router from '@/router'
@@ -10,6 +11,27 @@ Vue.config.productionTip = false
 Vue.component(btnX.name, btnX)
 
 Vue.use(AudioPlugin)
+
+// Matomo
+Vue.use(VueMatomo, {
+  host: 'https://matomo.ikotema.digital',
+  siteId: 4,
+  // Enables automatically registering pageviews on the router
+  router: router,
+  // Enables link tracking on regular links. Note that this won't
+  // work for routing links (ie. internal Vue router links)
+  // Default: true
+  enableLinkTracking: true,
+  // Require consent before sending tracking information to matomo
+  // Default: false
+  requireConsent: false,
+  // Whether to track the initial page view
+  // Default: true
+  trackInitialView: true,
+  // Whether or not to log debug information
+  // Default: false
+  debug: false
+})
 
 // Allows calling function when clicked outside of an element
 // Usefull for modal and styled select
