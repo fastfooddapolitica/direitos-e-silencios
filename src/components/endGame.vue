8823e38b642ad829d11323035bbf3517a0bbd8ca
AndresMRM <andres@inventati.org>
2019-02-21 21:32:27 -0300

    link questionário

--- src/components/endGame.vue
+++ src/components/endGame.vue
@@ -5,6 +5,7 @@
     <btn-x @click="$emit('tryAgain')" v-if="!won">Tentar de novo</btn-x>
     <btn-x @click="$emit('flipCards')">Revelar cartas</btn-x>
     <btn-x @click="$emit('restartGame')">Novo jogo</btn-x>
+    <p>Por favor! Responda ao nosso <a href="https://docs.google.com/forms/d/e/1FAIpQLSeVjVvijesilKEJkVz6wn-3LLrBbD26jFAZZuPJ6-HYrEa3wQ/viewform" target="_blank" rel="noopener noreferrer">questionário</a>!</p>
   </div>
 </template>
 
@@ -36,3 +37,10 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+@import '@/vars.scss';
+a {
+  color: $sec-color;
+}
+</style>
